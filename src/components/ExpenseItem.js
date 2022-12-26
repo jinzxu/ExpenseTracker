3bc9ca795d015664a402ec35a8a370ab6e6e9f39
Jason <jinzxu@ucalgary.ca>
2022-12-25 17:33:19 -0700

    Add Expenses Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,12 +5,13 @@ function ExpenseItem(props) {
     const expenseDate = props.date;
     const expenseTitle = props.title;
     const expenseAmount = props.amount;
+    console.log(expenseTitle)
     return (
         <div className="expense-item">
             <ExpenseDate date={expenseDate}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">{expenseAmount}</div>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
